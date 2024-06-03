7546ca4ff766c40f5f398aa2f44e9eaf293b33f0
adam147g <adasadas223@wp.pl>
2024-06-03 12:43:24 +0200

    Customized moving dot

--- ShipOptimizationApp/src/moj_kod.jl
+++ ShipOptimizationApp/src/moj_kod.jl
@@ -1,14 +1,13 @@
 using Plots
 T = 24
 
-
 # Funkcje definiujące składowe wektorów
 function vx_custom(x, y, t, T)
-    return (x-30)/30*sin(2 * π *t/T)+(y-40)/60
+    return (x-30)/30*sin(2 * π *t/T) + (y-40)/60
 end
 
 function vy_custom(x, y, t, T)
-    return (0.3*x+0.1*y) *cos(2 * π *t/T)
+    return (0.3*x + 0.1*y) * cos(2 * π *t/T)
 end
 
 # Zakresy dla x, y, t
@@ -22,14 +21,35 @@ grid_points = collect(Iterators.product(x_range, y_range))
 # Inicjalizacja listy przechowującej wektory
 quiver_plots = []
 
+# trzeba wywoływać za pomocą terminala include("moj_kod.jl")!!!
+# zapisuje jako gif
+
+function customDestination()
+    print("Enter the values of x and y separated by a space: ")
+    inputs = split(readline())
+    
+    x = parse(Int, inputs[1])
+    y = parse(Int, inputs[2])
+    return x, y
+end
+
+# Wczytaj cel
+destx, desty = customDestination()
+println("Destination coordinates: x = $destx, y = $desty")
+
 # Dla każdego punktu na siatce obliczamy wektor
 for t in t_range
     vx_values = [vx_custom(x, y, t, T) for (x, y) in grid_points]
     vy_values = [vy_custom(x, y, t, T) for (x, y) in grid_points]
     
-    quiver_plot = plot(xlim=(-10,10), ylim=(-10,10), xlabel="x", ylabel="y", title="Pole Wektorowe dla t = $t")
-    quiver!(quiver_plot, [p[1] for p in grid_points], [p[2] for p in grid_points], quiver=(vx_values, vy_values))
+    quiver_plot = plot(xlim=(-10,10), ylim=(-10,10), xlabel="x", ylabel="y", title="Pole Wektorowe dla t = $t, T = $T")
+    quiver!(quiver_plot, [p[1] for p in grid_points], [p[2] for p in grid_points], quiver=(vx_values, vy_values), color=:blue)
     
+    # Dodajemy czarny punkt poruszający się od (-10,0) do punktu docelowego (destx, desty)
+    x_point = -10 + (destx + 10) * t / T
+    y_point = desty * t / T
+    scatter!(quiver_plot, [x_point], [y_point], color=:red, markersize=4)
+
     push!(quiver_plots, quiver_plot)
 end
 
