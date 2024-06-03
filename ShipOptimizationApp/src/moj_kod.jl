3ae43f7a3fdc7eca0a8bd61a9b538e04e44c2d9b
adam147g <adasadas223@wp.pl>
2024-06-03 22:01:14 +0200

    Showing dot, from field and summary velocity

--- ShipOptimizationApp/src/moj_kod.jl
+++ ShipOptimizationApp/src/moj_kod.jl
@@ -42,13 +42,35 @@ for t in t_range
     vx_values = [vx_custom(x, y, t, T) for (x, y) in grid_points]
     vy_values = [vy_custom(x, y, t, T) for (x, y) in grid_points]
     
-    quiver_plot = plot(xlim=(-10,10), ylim=(-10,10), xlabel="x", ylabel="y", title="Pole Wektorowe dla t = $t, T = $T")
+    quiver_plot = plot(xlim=(-10,10), ylim=(-10,10), xlabel="x", ylabel="y", title="Pole Wektorowe dla t = $t, T = $T", legend=false)
     quiver!(quiver_plot, [p[1] for p in grid_points], [p[2] for p in grid_points], quiver=(vx_values, vy_values), color=:blue)
     
     # Dodajemy czarny punkt poruszający się od (-10,0) do punktu docelowego (destx, desty)
-    x_point = -10 + (destx + 10) * t / T
-    y_point = desty * t / T
-    scatter!(quiver_plot, [x_point], [y_point], color=:red, markersize=4)
+    vx = (destx + 10) / T
+    vy = desty / T
+
+    local x_point = -10 + vx * t
+    local y_point = vy * t
+
+    scatter!(quiver_plot, [x_point], [y_point], color=:red, markersize=5)
+
+    quiver!(quiver_plot, [x_point], [y_point], quiver=([vx], [vy]), color=:green, linewidth=2)
+
+    # Obliczamy prędkość kropek w punkcie (x_point, y_point)
+    vx_point = vx_custom(x_point, y_point, t, T)
+    vy_point = vy_custom(x_point, y_point, t, T)
+
+    # Dodajemy strzałkę będącą przedstawieniem prędkości kropski
+    quiver!(quiver_plot, [x_point], [y_point], quiver=([vx_point], [vy_point]), color=:black, linewidth=2)
+
+    local vx_sum = vx + vx_point
+    local vy_sum = vy + vy_point
+
+    quiver!(quiver_plot, [x_point], [y_point], quiver=([vx_sum], [vy_sum]), color=:orange, linewidth=2)
+
+    annotate!(quiver_plot, 0.5, -12, text("vx = $vx, vy = $vy", :left, 8))
+    annotate!(quiver_plot, 0.5, -13, text("vx_point = $vx_point, vy_point = $vy_point", :left, 8))
+    annotate!(quiver_plot, 0.5, -14, text("vx_sum = $vx_sum, vy_sum = $vy_sum", :left, 8))
 
     push!(quiver_plots, quiver_plot)
 end
