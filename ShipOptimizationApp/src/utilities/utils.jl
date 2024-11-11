91aebba2684a96293421707dd4b86211ac15860e
Xavvee <hkabzinski@studnet.agh.edu.pl>
2024-11-11 11:54:19 +0100

    Changed output speed to constant

--- ShipOptimizationApp/src/utilities/utils.jl
+++ ShipOptimizationApp/src/utilities/utils.jl
@@ -10,7 +10,7 @@ module Utils_Module
     end
 
     # Function to calculate the correct ship direction given current and destination vectors
-    function calculate_ship_direction(vc::Vector{T}, vd::Vector{T}, vs_speed::T) where T
+    function calculate_ship_vector(vc::Vector{T}, vd::Vector{T}, vs_speed::T) where T
         # Sprawdzenie, czy vs_speed jest większe od długości wektora vc
         if vs_speed <= norm(vc)
             return [-vc[1], -vc[2]]
@@ -34,47 +34,33 @@ module Utils_Module
         return vs_direction
     end
 
-
-end
-using LinearAlgebra
-
-function normalize(v::Vector{T}) where T
-    norm_v = sqrt(sum(v.^2)) 
-    if norm_v == 0
-        error("Cannot normalize a zero vector")
+    # Function to calculate the vector the ship should generate to achieve a target direction and speed
+    function calculate_ship_direction(vc::Vector{T}, vd::Vector{T}, max_speed::T) where T
+        # Normalize the destination direction
+        direction_to_destination = normalize(vd)
+        # Target velocity vector (in the desired direction with the desired speed)
+        target_velocity = max_speed * direction_to_destination
+        
+        # Compute the required ship-generated vector to achieve the target velocity when added to the current velocity
+        required_vector = target_velocity - vc
+        return required_vector
     end
-    return v / norm_v
-end
-# Function to calculate the vector the ship should generate to achieve a target direction and speed
-function calculate_ship_vector(vc::Vector{T}, vd::Vector{T}, max_speed::T) where T
-    # Normalize the destination direction
-    direction_to_destination = normalize(vd)
-    print("field ")
-    println(vc)
-    # Target velocity vector (in the desired direction with the desired speed)
-    target_velocity = max_speed * direction_to_destination
-    print("wypadkowa ")
-    println(target_velocity)
-    
-    # Compute the required ship-generated vector to achieve the target velocity when added to the current velocity
-    required_vector = target_velocity - vc
-    print("ship ")
-    println(required_vector)    
-    return required_vector
+
 end
 
 
-# Example vectors
-vc = [-2.5, 3.0]  # Current velocity vector
-vd = [7.0, 4.0]   # Desired velocity vector (toward the destination)
-vs_speed = 3.0    # Ship's constant speed
 
-# Calculate the ship's velocity direction
-vs = calculate_ship_vector(vc, vd, vs_speed)
+# # Example vectors
+# vc = [-2.5, 3.0]  # Current velocity vector
+# vd = [7.0, 4.0]   # Desired velocity vector (toward the destination)
+# vs_speed = 3.0    # Ship's constant speed
 
-println("The ship should go in the direction: $vs")
+# # Calculate the ship's velocity direction
+# vs = calculate_ship_vector(vc, vd, vs_speed)
 
-println("$(vs[1]+vc[1]), $(vs[2]+vc[2])")
-println("$((vs[1]+vc[1])^2), $((vs[2]+vc[2])^2)")
-println("$(sqrt((vs[1]+vc[1])^2+(vs[2]+vc[2])^2))")
+# println("The ship should go in the direction: $vs")
+
+# println("$(vs[1]+vc[1]), $(vs[2]+vc[2])")
+# println("$((vs[1]+vc[1])^2), $((vs[2]+vc[2])^2)")
+# println("$(sqrt((vs[1]+vc[1])^2+(vs[2]+vc[2])^2))")
 
