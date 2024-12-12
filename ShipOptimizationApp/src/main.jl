f4d3d4881058f4b03d8aa62c6395c684e955283b
adam147g <adasadas223@wp.pl>
2024-12-12 19:54:13 +0100

    Upload for plots and gifs

--- ShipOptimizationApp/src/main.jl
+++ ShipOptimizationApp/src/main.jl
@@ -57,12 +57,12 @@ module Main_Module
         gif(anim, "vector_field.gif", fps=3)
     end
 
-    ships1, evolved_ships = simulate_without_gif()
+    # ships1, evolved_ships = simulate_without_gif()
+    simulate_with_gif()
 
-    # simulate_with_gif()
-    ships, g, node_positions = Simulation_Module.simulate(T)
+    # ships, g, node_positions = Simulation_Module.simulate(T)
 
-    result_ships = Evolution_Algorithm_Module.evolve_with_evolutionary(ships, g, node_positions, 10, 15, 10, 5)
+    # result_ships = Evolution_Algorithm_Module.evolve_with_evolutionary(ships, g, node_positions, 10, 15, 10, 5)
     # println("------------------")
     # for ship in result_ships
     #     println("Fixed: $(ship.path) -> $(ship.finish_time) | $(ship.fuel_consumption)")
@@ -73,6 +73,8 @@ module Main_Module
             println("Number of ships in $(labels[i]): $(length(ships_lists[i]))")
         end
     
+        # Pierwszy wykres dla wszystkich statków
+        
         pl = plot()
         colors = [:red, :green, :blue]
         
@@ -82,11 +84,47 @@ module Main_Module
                      label=labels[i], color=colors[i])
         end
     
-        xlabel!("Finish Time")
-        ylabel!("Fuel Consumption")
-        display(pl)
+        xlabel!("Finish Time [h]")
+        ylabel!("Fuel Consumption [100 000 l]")
+        # display(pl)
+        savefig("bigThree.png")
+
+        # Find the fastest ship (with the minimum finish time)
+        all_ships = vcat(ships_lists...)
+        pl_pareto = plot()
+
+        # Znajdź punkty Pareto
+        pareto_ships = Simulation_Module.find_pareto_points(all_ships)
+
+        # Dodaj wszystkie statki do wykresu
+        scatter!(pl_pareto, 
+                map(x -> x.finish_time, all_ships), 
+                map(x -> x.fuel_consumption, all_ships), 
+                label="All Paths", color=:gray, alpha=0.5)
+
+        # Dodaj punkty Pareto do wykresu
+        scatter!(pl_pareto, 
+                map(x -> x.finish_time, pareto_ships), 
+                map(x -> x.fuel_consumption, pareto_ships), 
+                label="Pareto Paths", color=:blue, markersize=5)
+
+        xlabel!("Finish Time [h]")
+        ylabel!("Fuel Consumption [100 000 l]")
+        # display(pl_pareto)
+        savefig("paretoPoints.png")
+
+        println("-------------------------------")
+        fastest_ship_index = argmin(map(x -> x.finish_time, all_ships))
+        fastest_ship = all_ships[fastest_ship_index]
+        println("Fastest ship: Finish Time = $(fastest_ship.finish_time), \n Continuous Path = $(fastest_ship.continuous_path), \n Fuel Consumption = $(fastest_ship.fuel_consumption)")
+
+        println("-------------------------------")
+        # Find the ship with the minimal fuel consumption
+        minimal_fuel_ship_index = argmin(map(x -> x.fuel_consumption, all_ships))
+        minimal_fuel_ship = all_ships[minimal_fuel_ship_index]
+        println("Minimal fuel consumption ship: Fuel Consumption = $(minimal_fuel_ship.fuel_consumption), \n Continuous Path = $(minimal_fuel_ship.continuous_path), \n Finish Time = $(minimal_fuel_ship.finish_time)")
     end
     
-    display_ship_plots([ evolved_ships, result_ships], [ "Evoluated Ships", "Result Ships"])
+    # display_ship_plots([ evolved_ships, result_ships], [ "Naive Algorithm Paths", "Genetic Algorithm Paths"])
 
 end
--- ShipOptimizationApp/src/simulation/simulation.jl
+++ ShipOptimizationApp/src/simulation/simulation.jl
@@ -26,20 +26,21 @@ module Simulation_Module
         x_min, x_max = first(x_range), last(x_range)
         y_min, y_max = first(y_range), last(y_range)
 
-        quiver_plot = plot(xlim=(x_min, x_max), ylim=(y_min, y_max), xlabel="x", ylabel="y", title="Pole Wektorowe dla time = $time, T = $T", legend=false, bottom_margin=30px, right_margin=30px)
+        quiver_plot = plot(xlim=(x_min, x_max), ylim=(y_min, y_max), xlabel="Longitude [10km]", ylabel="Latitude [10km]", title="Pole Wektorowe dla time = $time, T = $T", legend=false, bottom_margin=30px, right_margin=30px)
         
         quiver!(quiver_plot, [p[1] for p in grid_points], [p[2] for p in grid_points], quiver=(vx_values, vy_values), color=:blue)
         
-        scatter!(quiver_plot, x_curr, y_curr, color=:red, markersize=8)
         
-        labels = ["$i" for i in 1:length(x_curr)]
+        
+        # labels = ["$i" for i in 1:length(x_curr)]
+        labels = ["fastest", "min_fuel"]
         
         # Positions relocation on the plot
-        dx = 1
-        dy = 1
+        dx = 2
+        dy = 2
 
         for (x, y, label) in zip(x_curr, y_curr, labels)
-            annotate!(quiver_plot, x + dx, y + dy, text(label, 10, :black))
+            annotate!(quiver_plot, x + dx, y + dy, text(label, 12, :black))
         end
 
         for e in edges(g)
@@ -55,7 +56,7 @@ module Simulation_Module
         for (_, (x_i, y_i)) in enumerate(node_positions)
             scatter!(quiver_plot, [x_i], [y_i], color=:black, markersize=3, label=false)
         end
-
+        scatter!(quiver_plot, x_curr, y_curr, color=:red, markersize=8)
         return quiver_plot
     end
 
@@ -89,14 +90,16 @@ module Simulation_Module
 
         path1 = [1, 4, 9, 20, 37, 41, 52, 57, 59, 60] #42
         path2 = [1, 4, 7, 20, 27, 42, 53, 56, 59, 60] #31
-        path3 = [1, 4, 9, 15, 33, 43, 49, 56, 59, 60] #53.5
-
+        # path3 = [1, 4, 9, 15, 33, 43, 49, 56, 59, 60] #53.5
         ship1 = Ship_Module.Ship(x_start, y_start, x_finish, y_finish, max_speed, path1, time_step)
         ship2 = Ship_Module.Ship(x_start, y_start, x_finish, y_finish, max_speed, path2, time_step)
-        ship3 = Ship_Module.Ship(x_start, y_start, x_finish, y_finish, max_speed, path3, time_step)
+        # ship3 = Ship_Module.Ship(x_start, y_start, x_finish, y_finish, max_speed, path3, time_step)
+        ship1.continuous_path = [(-7.0, 17.0), (-3.1870212011632515, 14.296814971173138), (0.6888844479022929, 11.67349863686744), (4.93585851300315, 9.521153753683485), (8.406910280420265, 6.383984415747088), (11.297534654760321, 2.5101187712128876), (14.189966951505207, -1.361452202576725), (18.62775586525996, -3.271609008623045), (22.333333333333332, -6.111111111111111), (26.0, -9.0)]
+        ship2.continuous_path = [(-7.0, 17.0), (-4.015911286500617, 13.244762170552635), (1.7522897415828866, 13.023205355769733), (9.998707190970276, 15.947077075718685), (12.621701101075779, 11.733526611194474), (14.701287475669334, 6.830266582366637), (17.207458227318362, 2.468440570570742), (19.53385587354616, -2.121558998105943), (22.333333333333332, -6.111111111111111), (26.0, -9.0)]
+
         push!(ships, ship1)
         push!(ships, ship2)
-        push!(ships, ship3)
+        # push!(ships, ship3)
 
         while any(ship -> ship.current_node_index < length(ship.path), ships)
             time_generated = Time_Generator_Module.iterate(time_generator)
@@ -115,9 +118,10 @@ module Simulation_Module
                     g, node_positions)
 
             for ship in ships
+                points = ship.continuous_path
                 time_consumed = 0.0
 
-                Ship_Module.update_field_speed!(ship, Field_Module.v_custom(ship.position_x, ship.position_y, mod(time, 24), T, "x"), Field_Module.v_custom(ship.position_x, ship.position_y, mod(time,24), T, "y"))
+                Ship_Module.update_field_speed!(ship, Field_Module.v_custom(ship.position_x, ship.position_y, mod(time, 24), T, "x"), Field_Module.v_custom(ship.position_x, ship.position_y,  mod(time, 24), T, "y"))
 
                 quiver!(quiver_plot, [ship.position_x], [ship.position_y], quiver=([ship.field_speed_x], [ship.field_speed_y]), color=:black, linewidth=2)
     
@@ -125,7 +129,7 @@ module Simulation_Module
                     continue
                 end
 
-                next_x, next_y = node_positions[ship.path[ship.current_node_index + 1]]
+                next_x, next_y = points[ship.current_node_index+1]
                 direction_x = next_x - ship.position_x
                 direction_y = next_y - ship.position_y
                 norm = sqrt(direction_x^2 + direction_y^2)
@@ -146,7 +150,7 @@ module Simulation_Module
                         ship.position_x = next_x
                         ship.position_y = next_y
                         if ship.current_node_index < length(ship.path) - 1
-                            tmp_next_x, tmp_next_y = node_positions[ship.path[ship.current_node_index + 2]]
+                            tmp_next_x, tmp_next_y = points[ship.current_node_index + 2]
                             tmp_direction_x = tmp_next_x - ship.position_x
                             tmp_direction_y = tmp_next_y - ship.position_y
 
@@ -173,7 +177,7 @@ module Simulation_Module
         
         println("$(ship1.path) -> $(ship1.finish_time)")
         println("$(ship2.path) -> $(ship2.finish_time)")
-        println("$(ship3.path) -> $(ship3.finish_time)")
+        # println("$(ship3.path) -> $(ship3.finish_time)")
     
         return quiver_plots
     end
